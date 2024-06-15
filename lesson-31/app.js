9ea004e4100a5986cfd3918c8eb785b15531dbf5
choi-pil-gyu <162441357+choi-pil-gyu@users.noreply.github.com>
2024-06-15 11:48:25 +0000

    30!!

--- lesson-31/app.js
+++ lesson-31/app.js
@@ -96,18 +96,15 @@ app.use((req, res, next) => {
  */
 
 // 애플리케이션에 Mongoose 설정
-const mongoose = require("mongoose"), // mongoose를 요청
-  dbName = "aaronkr";
+const mongoose = require("mongoose"); // mongoose를 요청
 
-// 데이터베이스 연결 설정
-mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb+srv://ut-node:1234@ut-node.kv6iahy.mongodb.net/?retryWrites=true&w=majority&appName=UT-NODE"
+);
 
-// 연결되면 메시지를 보냄
 const db = mongoose.connection;
 db.once("open", () => {
-  console.log(`Connected to ${dbName} MongoDB using Mongoose!`);
+  console.log("db is connect!");
 });
 
 /**
